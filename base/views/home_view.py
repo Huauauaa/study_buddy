f944c87dd72b7001abd050317e9e5174aec983c3
huaweiwei <harvey0379@163.com>
2021-10-31 18:56:10 +0800

    feat: Search

--- base/views/home_view.py
+++ base/views/home_view.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.db.models import Q
 
 from base.models.Room import Room
+from base.models.Topic import Topic
 
 
 def home(request):
-    rooms = Room.objects.all()
-    return render(request, 'base/home.html', {'rooms': rooms})
+    q = request.GET.get('q') or ''
+    topic_name = request.GET.get('topic_name') or ''
+    if topic_name:
+        rooms = Room.objects.filter(topic__name=topic_name)
+    else:
+        rooms = Room.objects.filter(
+            Q(topic__name__icontains=q) | Q(name__icontains=q)
+        )
+    topics = Topic.objects.all()
+
+    return render(
+        request,
+        'base/home.html',
+        {'rooms': rooms, 'topics': topics, 'room_count': rooms.count(), 'q': q},
+    )
